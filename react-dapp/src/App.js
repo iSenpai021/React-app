03fd366fce1a14cc6f361e97245657ffde571bdb
iSenpai021 <isenpai021@gmail.com>
2022-02-19 01:25:31 +0000

    created ERC20 token

--- react-dapp/src/App.js
+++ react-dapp/src/App.js
@@ -5,8 +5,8 @@ import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
 import Token from './artifacts/contracts/Token.sol/Token.json'
 
 // Update with the contract address logged out to the CLI when it was deployed 
-const greeterAddress = " 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-const tokenAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+const greeterAddress = " 0x5FbDB2315678afecb367f032d93F642f64180aa3"
+const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 function App() {
   // store greeting in local state
@@ -37,7 +37,7 @@ function App() {
       const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
       const transaction = await contract.transfer(userAccount, amount);
       await transaction.wait();
-      console.log('${amount} Coins successfully sent to ${userAccount}');
+      console.log(amount + ' Coins successfully sent to ' + userAccount);
     }
   }
 
